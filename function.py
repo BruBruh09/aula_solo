9f7bf638936e2326cfd2a6422156be6b8659de63
brubruh <00001111113804sp@al.educacao.sp.ov.br>
2025-04-29 12:13:00 -0300

    seu nome e idade

---
+++ function.py
@@ -0,0 +1,30 @@
+def soma ():
+    num01 = 25
+    num02 = 31
+
+    resultado = num01 + num02
+
+    return resultado
+
+def somarDoisNumeros(nota01, nota02):
+    resultado = nota01 + nuta02
+
+    return print(resultado)
+
+#somarDoisNumeros(10, 22)
+
+nota03 = 200
+nota04 =145
+
+def multiplicarDoisNUmeros (nota03, nota04):
+    resultado = nota03 * nota04
+
+    return print(resultado)
+
+def cadastrar (nome, idade):
+    data = {
+        "nome": nome,
+        "idade": idade
+    }
+    return print(data)
+cadastrar("bruno", 16)
